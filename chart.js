f7ed09e0a667cd42cf4322bf312df805c07b02e9
carinaramos <cgramos50@gmail.com>
2021-12-03 16:01:28 -0600

    sbar chart for new format

--- chart.js
+++ chart.js
@@ -1,9 +1,8 @@
 const layout = {
-    width: 1000,
+    width: 2200,
     height: 510,
-    chartWidth: 950,
-    topChartHeight: 225,
-    bottomChartHeight: 225,
+    chartWidth: 600,
+    chartHeight: 400,
     marginTop: 20,
     marginBottom: 40,
     marginLeft: 40,
@@ -11,20 +10,30 @@ const layout = {
     bumper: 10
   };
 
+const roundNames = {
+    0: "Round of 64",
+    1: "Round of 32",
+    2: "Sweet Sixteen",
+    3: "Elite 8",
+    4: "Final Four",
+    5: "Championship",
+    6: "Winner"
+}
 
 
 async function ready() {
     var records = await d3.json("records.json");
     var streaks = await d3.json("streaks_by_team.json");
-    var selectedSchools = ["Georgetown", "Michigan"];
-
+    var selectedSchools = ["Georgetown"];
+    var allSchools = Object.keys(streaks)
+    console.log(allSchools.length)
     function filterData() {
         return records.filter(record => selectedSchools.includes(record.name));
     }
     var filteredRecords = filterData();
     console.log(filteredRecords.length);
 
-    // create background paper for plot
+    // create\ background paper for plot
     let svg = d3.select("#vis").append("svg");
     svg.attr("id", "my-vis")
         .attr("width", layout.width)
@@ -32,7 +41,7 @@ async function ready() {
         .attr("viewBox", [0, 0, layout.width, layout.height].join(" "));
 
     
-    function drawMarks(schoolName) {
+    function drawColumns(schoolName) {
         // var schoolRecords = records.filter(record => record.name === schoolName);
         var schoolStreaks = streaks[schoolName];
         console.log(schoolStreaks)
@@ -40,64 +49,25 @@ async function ready() {
         var schoolRecords = []
         for (var i=0 ; i<schoolStreaks.length ; i++) {
             schoolRecords = schoolStreaks[i]
-            // NUM WINS
-            // line connecting points
-        svg.append("path")
-            .datum(schoolRecords, d => d["id"])
-            .attr("transform", `translate(${layout.marginLeft},${layout.marginTop})`)
-            .attr("class", "connector")
-            .attr("fill", "none")
-            .attr("stroke", schoolColor)
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-            .x(d => xScale(d["year"]))
-            .y(d => yScale(d["wins"]))
-            )
-            // mark group layout
-            let markGroupWins = svg.append("g")
-            .attr("id", "marks")
-            .attr("transform", `translate(${layout.marginLeft},${layout.marginTop})`);
-            // mapping data to actual marks
-            let winMarks = markGroupWins.selectAll("circle").data(schoolRecords, d => d["id"]);
-            winMarks.join(enter => enter.append("circle"))
-            .attr("cx", d => xScale(d["year"]))
-            .attr("cy", d => yScale(d["wins"]))
-            .attr("r", d => d["wins"] === 6? 10 : 4)
-            .attr("fill", d => d["color"])
-            .attr("opacity", 0.7);
-            
-            // SEEDS
-            // line connecting points
-            svg.append("path")
-            .datum(schoolRecords, d => d["id"])
-            .attr("transform", `translate(${layout.marginLeft},${layout.marginTop + layout.topChartHeight + 30})`)
-            .attr("class", "connector")
-            .attr("fill", "none")
-            .attr("stroke", schoolColor)
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-            .x(d => xScale(d["year"]))
-            .y(d => yScaleBottom(d["seed"]))
-            )
-            // mark group layout
-            let markGroupSeeds = svg.append("g")
-            .attr("id", "marks")
-            .attr("transform", `translate(${layout.marginLeft},${layout.marginTop + layout.topChartHeight + 30})`);
-            // mapping data to actual marks
-            let seedMarks = markGroupSeeds.selectAll("circle").data(schoolRecords, d => d["id"]);
-            seedMarks.join(enter => enter.append("circle")).join(exit => exit.remove())
-            .attr("cx", d => xScale(d["year"]))
-            .attr("cy", d => yScaleBottom(d["seed"]))
-            .attr("r", 4)
-            .attr("fill", d => d["color"])
-            .attr("opacity", 0.7);
+
+            svg.selectAll(".bar")
+                .data(schoolRecords, d => d["id"])
+                .enter().append("rect")
+                .attr("transform", `translate(${layout.marginLeft},${layout.marginTop})`)
+                .attr("class", "bar")
+                .attr("fill", "steelblue")
+                .attr("opacity", 0.7)
+                .attr("x", d => xScale(d["seed"]))
+                .attr("y", d => yScale(d["wins"]))
+                .attr("width", 10)
+                .attr("height", function(d) { return layout.chartHeight - yScale(d.wins); });
         }
     }
 
 
     function handleSchoolClick(event) {
-        svg.selectAll("circle").remove()
-        svg.selectAll(".connector").remove()
+        svg.selectAll("rect").remove()
+        // svg.selectAll(".connector").remove()
         var btn = event.target;
         var schoolName = btn.innerHTML;
         if (btn.style.backgroundColor !== "white") {
@@ -115,17 +85,20 @@ async function ready() {
         }
         console.log(selectedSchools);
         for (var i=0; i<selectedSchools.length; i++){ 
-            drawMarks(selectedSchools[i]);
+            drawColumns(selectedSchools[i]);
         }
         
     }
     
-
+    function range(start, end) {
+        return Array(end - start + 1).fill().map((_, idx) => start + idx)
+    }
     // SCALE FUNCTIONS
     // y scale and axis for wins by team
+    let yData =  range(0, 6);
     let yScale = d3.scaleLinear()
-        .domain([0 , 6])
-        .range([layout.topChartHeight, 0]);
+        .domain([d3.min(yData) - 0.9, d3.max(yData)])
+        .range([layout.chartHeight, 0]);
     let yAxis = svg.append("g")
         .attr("transform", `translate(${layout.marginLeft},${layout.marginTop})`)
         .call(d3.axisLeft(yScale))
@@ -139,37 +112,29 @@ async function ready() {
         .attr("font-size", 14)
         .attr("fill", "dimgray");
     
-    // y scale and axis for seeds by team
-    let yScaleBottom = d3.scaleLinear()
-        .domain([16, 1])
-        .range([layout.bottomChartHeight, 0]);
-    let yAxisBottom = svg.append("g")
-        .attr("transform", `translate(${layout.marginLeft},${layout.marginTop + layout.topChartHeight + 30})`)
-        .call(d3.axisLeft(yScaleBottom))
-        .call(d3.axisLeft(yScaleBottom).ticks(16));
-    yAxisBottom.selectAll("text").attr("fill", "gray");
-    yAxisBottom.selectAll("line, .domain").attr("stroke", "gray");
-    svg.append("text")
-        .attr("transform", `translate(${layout.marginLeft + 5},${layout.marginTop + layout.topChartHeight + layout.bottomChartHeight + 35})`)
-        .text("Seed")
-        .attr("font-size", 14)
-        .attr("fill", "dimgray");
     
     function range(start, end) {
         return Array(end - start + 1).fill().map((_, idx) => start + idx)
-        }
-    let xData =  range(1985, 2016);
+    }
+    let xData =  range(1, 16);
     let xScale = d3.scaleLinear()
-        .domain([d3.min(xData) - 0.5, d3.max(xData) + 0.5])
+        .domain([d3.min(xData) - 1, d3.max(xData)])
         .range([0, layout.chartWidth]);
     let xAxis = svg.append("g")
-        .attr("transform", `translate(${layout.marginLeft},${layout.marginTop + layout.topChartHeight})`)
+        .attr("transform", `translate(${layout.marginLeft},${layout.marginTop + layout.chartHeight})`)
         .call(d3.axisBottom(xScale))
-        .call(d3.axisBottom(xScale).ticks(32).tickFormat(d3.format("d")));
+        .call(d3.axisBottom(xScale).ticks(16).tickFormat(d3.format("d")));
 
     xAxis.selectAll("text").attr("fill", "gray");
     xAxis.selectAll("line, .domain").attr("stroke", "gray");
     
+    // x axis title
+    svg.append("text")
+      .attr("transform", `translate(${layout.width - layout.marginRight},${layout.height - layout.marginBottom + 15})`)
+      .text("Seed")
+      .attr("text-anchor", "end")
+      .attr("font-size", 14)
+      .attr("fill", "dimgray")
     // create a button for each school
     var listDiv = document.getElementById('list');
     var counter = 0;
@@ -186,9 +151,8 @@ async function ready() {
         counter += 1;                                
     }
     for (var i=0; i < selectedSchools.length; i++) {
-        console.log(selectedSchools[i]);
-        // console.log(streaks[selectedSchools[i]])
-        drawMarks(selectedSchools[i]);
+        drawColumns(selectedSchools[i]);
+        // drawColumns(allSchools[i]);
     }
 };
 
