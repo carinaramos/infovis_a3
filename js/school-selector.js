cc8d649cd37078fd4923d52ea662d22dbf114365
Alex <alexgold1100@gmail.com>
2021-12-05 16:19:50 -0600

    Clear button is functional

--- js/school-selector.js
+++ js/school-selector.js
@@ -1,12 +1,23 @@
 
-// create a button for each school
-var listDiv = document.getElementById('year-selector');
-for (var i=1985; i < 2017; i++) {
-    var opt = document.createElement('option');
-    // opt.classList = 'btn btn-outline-secondary';
-    opt.id = records[i]["name"];
-    // button.style.backgroundColor = ...
-    opt.innerHTML = records[i]["name"];
-    opt = handleSchoolClick;
-    listDiv.appendChild(opt);                             
+var clearButton = document.getElementById('clear-button');
+clearButton.onclick = function() {handleClear(this)};
+
+const handleClear = () => {
+    console.log("clearing all teams");
+    const numSchoolsSelected = selectedSchools.length;
+    for (let i=0; i < numSchoolsSelected; i++) {
+        updateSelectedSchools(selectedSchools[0]);
+    }
 }
+
+// // create a button for each school
+// var listDiv = document.getElementById('year-selector');
+// for (var i=1985; i < 2017; i++) {
+//     var opt = document.createElement('option');
+//     // opt.classList = 'btn btn-outline-secondary';
+//     opt.id = records[i]["name"];
+//     // button.style.backgroundColor = ...
+//     opt.innerHTML = records[i]["name"];
+//     opt = handleSchoolClick;
+//     listDiv.appendChild(opt);                             
+// }
